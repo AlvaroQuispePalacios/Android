652583c0390685d0a15e10a6579ed687a9b9c1ae
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-10-15 19:34:20 +0200

    Actividad 4.1 hecha

--- EjerciciosDeAsteroides/InterfazAsteroidesIdioma/app/src/main/java/com/alvaroquispe/interfazasteroides/MainActivity.java
+++ EjerciciosDeAsteroides/InterfazAsteroidesIdioma/app/src/main/java/com/alvaroquispe/interfazasteroides/MainActivity.java
@@ -1,6 +1,10 @@
 package com.alvaroquispe.interfazasteroides;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +13,39 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnSalir;
+    Button btnSobre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //
+        btnSobre = findViewById(R.id.btnSobre);
+        btnSobre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Hola")
+                        .setMessage("Alvaro Quispe Palacios")
+                        .setCancelable(true)
+                        .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        })
+                        .show();
+            }
+        });
+        //
+        btnSalir = findViewById(R.id.btnSalir);
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
