e447b16d410c5c12cbeddecb4ea90c8f0c5333ef
AlvaroQP <alonsol1569@gmail.com>
2024-10-27 22:07:18 +0100

    activdad 4.3 hecha

--- EjerciciosDeAsteroides/InterfazAsteroidesIdioma/app/src/main/java/com/alvaroquispe/interfazasteroides/MainActivity.java
+++ EjerciciosDeAsteroides/InterfazAsteroidesIdioma/app/src/main/java/com/alvaroquispe/interfazasteroides/MainActivity.java
@@ -4,10 +4,13 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,32 +19,23 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     Button btnSalir;
     Button btnSobre;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         //
         btnSobre = findViewById(R.id.btnSobre);
         btnSobre.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mostrarViewSobre(null);
-
-//                new AlertDialog.Builder(MainActivity.this)
-//                        .setTitle("Hola")
-//                        .setMessage("Alvaro Quispe Palacios")
-//                        .setCancelable(true)
-//                        .setPositiveButton("yes", new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialogInterface, int i) {
-//                                dialogInterface.cancel();
-//                            }
-//                        })
-//                        .show();
             }
         });
         //
+
         btnSalir = findViewById(R.id.btnSalir);
         btnSalir.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,11 +43,30 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id==R.id.preferences){
+        //arrancar activitat preferències
+        }
+        if (id == R.id.about){
+        //arrancar activitat sobre...
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     // ---------------------------------------------------------
