c02238e7e794f6883df1ea596981fee97b2a724c
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2025-03-10 19:41:50 +0100

    10-03-2025

---
+++ PruebaComponse/app/src/main/java/org/example/pruebacomponse/MainActivity.kt
@@ -0,0 +1,89 @@
+package org.example.pruebacomponse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.example.pruebacomponse.ui.theme.PruebaComponseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PruebaComponseTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Alvaro",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Row {
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .padding(8.dp),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Text("Home")
+            }
+            Button(
+                onClick = {},
+                modifier = Modifier.padding(8.dp),
+                shape = RoundedCornerShape(8.dp)
+
+            ) {
+                Text("About")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PruebaComponseTheme {
+        Greeting("Android")
+    }
+}
+
+fun main() {
+    val numero = 24
+    println("Hola mundo")
+    println("Edad $numero")
+}
