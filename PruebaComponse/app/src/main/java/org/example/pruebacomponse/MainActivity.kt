4b6db0494409278b0576cc58d6d5b90ea29e58c6
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2025-03-11 17:37:51 +0100

    11-03-2025

--- PruebaComponse/app/src/main/java/org/example/pruebacomponse/MainActivity.kt
+++ PruebaComponse/app/src/main/java/org/example/pruebacomponse/MainActivity.kt
@@ -1,9 +1,11 @@
 package org.example.pruebacomponse
 
 import android.os.Bundle
+import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,14 +13,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.example.pruebacomponse.ui.theme.PruebaComponseTheme
@@ -41,6 +48,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MenuBar() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+
+    ) {
+        Text(text = "Menubar")
+        Row {
+            Button(
+                onClick = {},
+                shape = RoundedCornerShape(8.dp),
+            )
+            {
+                Image(
+                    painter = painterResource(R.drawable.home),
+                    contentDescription = "Home"
+                )
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Menu() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            MenuBar()
+        }
+    ) { innerPadding ->
+        Text(text = "Contenido", modifier = Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Row(
         modifier = Modifier
@@ -53,14 +98,24 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             text = "Hello $name!",
             modifier = modifier
         )
-        Row {
+        Row(
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Button(
                 onClick = {},
-                modifier = Modifier
-                    .padding(8.dp),
-                shape = RoundedCornerShape(8.dp)
-            ) {
-                Text("Home")
+                shape = RoundedCornerShape(8.dp),
+            )
+            {
+                Row(
+                )
+                {
+                    Image(
+                        painter = painterResource(R.drawable.home),
+                        contentDescription = "Home"
+                    )
+                    Text("Home")
+                }
             }
             Button(
                 onClick = {},
@@ -78,12 +133,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PruebaComponseTheme {
-        Greeting("Android")
+//        Greeting("Android")
+        Menu()
     }
-}
-
-fun main() {
-    val numero = 24
-    println("Hola mundo")
-    println("Edad $numero")
 }
